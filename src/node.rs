54de9e9488b49f37ed0d6c9cae0fe9308d54eaf8
CrocdileChan <CrocdileChan@github.com>
2019-10-29 20:17:09 +0800

    define nodeType

--- src/node.rs
+++ src/node.rs
@@ -1,20 +1,51 @@
-enum Node {
-    Node4 {
-        key: [char; 4],
-        children: [Node; 4],
-    },
-    Node16 {
-        key: [char; 16],
-        children: [Node; 16],
-    },
-    Node48 {
-        key: [char; 256],
-        children: [Node; 48],
-    },
-    Node256 {
-        children: [Node; 256],
-    },
+enum NodeType {
+    Node4,
+    Node16,
+    Node48,
+    Node256,
     Leaf,
 }
 
-impl Node {}
+impl NodeType {
+    fn max(self) -> usize {
+        match self {
+            NodeType::Node4 => NODE4MAX,
+            NodeType::Node16 => NODE16MAX,
+            NodeType::Node48 => NODE48MAX,
+            NodeType::Node256 => NODE256MAX,
+            NodeType::Leaf => 0,
+        }
+    }
+}
+
+const NODE4MIN: usize = 2;
+const NODE4MAX: usize = 4;
+
+const NODE16MIN: usize = 5;
+const NODE16MAX: usize = 16;
+
+const NODE48MIN: usize = 17;
+const NODE48MAX: usize = 48;
+
+const NODE256MIN: usize = 49;
+const NODE256MAX: usize = 256;
+
+const PREFIX_LEN: usize = 10;
+
+struct Node {
+    typ: NodeType,
+    key: [char],
+    children: [Node],
+    prefix: [char],
+}
+
+impl Node {
+    pub fn new(typ: NodeType) -> Box<Node> {
+        Box::from(Node {
+            typ,
+            key: [char; typ.max()],
+            children: [Node; typ.max()],
+            prefix: [char; PREFIX_LEN],
+        })
+    }
+}
