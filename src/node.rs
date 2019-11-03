0de5c620ff834bf39e61049631b7a91398a615e8
CrocdileChan <CrocdileChan@github.com>
2019-11-03 22:53:57 +0800

    box to ref

--- src/node.rs
+++ src/node.rs
@@ -12,31 +12,83 @@ const NODE256MAX: usize = 256;
 
 const PREFIX_LEN: usize = 10;
 
-struct Node {}
+enum ArtNodeType {
+    // key 4
+    // children 4
+    Node4,
 
-struct Node4 {
-    keys: [char; 4],
-    children: [Node; 4],
+    // key 16
+    // children 16
+    Node16,
+
+    // key 256
+    // children 48
+    Node48,
+
+    // children 256
+    Node256,
+
+    Leaf,
 }
 
-struct Node16 {
-    keys: [char; 16],
-    children: [Node; 16],
+struct Node<'a> {
+    typ: ArtNodeType,
+    key_char: &'a [char],
+    children: &'a [Node],
 }
 
-struct Node48 {
-    keys: [char; 256],
-    children: [Node; 48],
+impl Node {
+    fn new_node4(key_char: [char; 4]) -> Node {
+        Node {
+            typ: ArtNodeType::Node4,
+            key_char: &key_char,
+            children: &[],
+        }
+    }
+
+    fn new_node16(key_char: [char; 16]) -> Node {
+        Node {
+            typ: ArtNodeType::Node16,
+            key_char: &key_char,
+            children: &[],
+        }
+    }
+
+    fn new_node48(key_char: [char; 256]) -> Node {
+        Node {
+            typ: ArtNodeType::Node48,
+            key_char: &key_char,
+            children: &[],
+        }
+    }
+
+    fn new_node256() -> Node {
+        Node {
+            typ: ArtNodeType::Node256,
+            key_char: &[],
+            children: &[],
+        }
+    }
+
+    fn new_leaf_node(key_char: &[char]) -> Node {
+        Node {
+            typ: ArtNodeType::Leaf,
+            key_char,
+            children: &[],
+        }
+    }
+
+    fn is_leaf(&self) -> bool {
+        self.typ == ArtNodeType::Leaf
+    }
+
+    fn add_child(&self) {}
+
+    fn remove_child(&self) {}
+
+    fn find_child(&self, key: &[char]) {}
+
+    fn grow(&self) {}
+
+    fn is_full(&self) -> bool {}
 }
-
-struct Node256 {
-    children: [Node; 256],
-}
-
-fn new_node4() -> Node4 {}
-
-fn new_node16() -> Node16 {}
-
-fn new_node48() -> Node48 {}
-
-fn new_node256() -> Node256 {}
