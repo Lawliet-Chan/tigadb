928d32a96f0fb2a058219f9edaccb0d4446a27cc
CrocdileChan <CrocdileChan@github.com>
2019-12-26 23:08:57 +0800

    metadata

--- src/kv.rs
+++ src/kv.rs
@@ -3,16 +3,16 @@ use std::io;
 use std::u64;
 
 pub(crate) struct KV {
-    key_log: GroupLog,
-    value_log: GroupLog,
+    meta_log: GroupLog,
+    kv_log: GroupLog,
 }
 
 impl KV {
     #[inline]
     pub(crate) fn new(key_dir: &'static str, value_dir: &'static str, limit_per_file: u64) -> Self {
         KV {
-            key_log: GroupLog::new(key_dir, limit_per_file),
-            value_log: GroupLog::new(value_dir, limit_per_file),
+            meta_log: GroupLog::new(key_dir, limit_per_file),
+            kv_log: GroupLog::new(value_dir, limit_per_file),
         }
     }
 
@@ -23,11 +23,19 @@ impl KV {
         value: &[u8],
         fsync: bool,
     ) -> io::Result<(u8, u64, u64)> {
-        let value_pos = self.value_log.write(value, fsync)?;
-        //let v_off: [u8;8] = value_pos.1.to_be_bytes;
-        //let v_len: [u8;8] = value_pos.2.to_be_bytes;
-        //let key_and_vpos = value_pos.0 + v_off + v_len + key;
-        //self.key_log.write(key_and_vpos,fsync)
+        let dividing_point = key.len() as u64;
+        let kv_data = [*key, *value].concat().as_slice();
+        let kv_pos = self.kv_log.write(kv_data, fsync)?;
+
+        let kv_offset_u8: [u8; 8] = kv_pos.1.to_be_bytes;
+        let kv_len_u8: [u8; 8] = kv_pos.2.to_be_bytes;
+        let dividing_point_u8: [u8; 8] = dividing_point.to_be_bytes;
+        let metadata = [kv_pos.0, kv_offset_u8, kv_len_u8, dividing_point_u8]
+            .concat()
+            .as_slice();
+        self.meta_log.write(metadata, fsync)?;
+        kv_pos.1 += dividing_point;
+        Ok(kv_pos)
     }
 
     #[inline]
