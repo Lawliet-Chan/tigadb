f15a9fca9366a81c4337aa1fc81849825aa3149c
CrocdileChan <CrocdileChan@github.com>
2020-01-08 09:33:26 +0800

    add apply_ts and commit_ts

--- src/db.rs
+++ src/db.rs
@@ -3,6 +3,7 @@ use crate::kv::KV;
 use crate::option::Option;
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
+use std::time::SystemTime;
 
 pub struct DB {
     opt: Option,
@@ -12,15 +13,29 @@ pub struct DB {
     // and spawn one thread to execute all concurrent writing transactions.
     txn_id: AtomicUsize,
 
+    // When data has been stored in disk and apply into ART-tree, it would be recorded two timestamps
+    // because now the data can be read for user but the data maybe not in ART-tree yet.
+    /// commit_ts is the last timestamp when the data are stored in disk.
+    //commit_ts: SystemTime,
+
+    /// apply_ts is the last timestamp when the data are applied into ART-tree.
+    apply_ts: SystemTime,
+    // When commit_ts <= reading_request_ts < apply_ts,
+    // the reading ops will wait until reading_request_ts reaches apply_ts.
+    // Otherwise, just read in ART-tree.
+
     tree: Node,
     disk: KV,
 }
 
 impl DB {
     pub fn new(opt: Option) -> DB {
+        let now = SystemTime::now();
         DB {
             opt,
             txn_id: AtomicUsize::new(0),
+            //commit_ts: now,
+            apply_ts: now,
             tree: Node::new_node(ArtNodeType::Node4),
             disk: KV::new(opt.meta_dir, opt.kv_dir, opt.limit_per_file),
         }
